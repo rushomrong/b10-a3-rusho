158ece291c26ba5295d0e96d030173a6ccf3f0e5
rushomrong <rushomarak@gmail.com>
2024-08-22 02:59:53 +0600

    headr section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["*"],
   theme: {
     extend: {},
+    colors: {
+      "primary-r-color": ["#ABEF5F"],
+      "top-right-color": ["#1C1C1C"],
+      "text-white": ["#ffffff"],
+      "text-nav-active": ["#494949"],
+      "text-nav-regular": ["#737373"],
+      "bg-accent": ["#f1f5f9"],
+    },
+
+    backgroundImage: {
+      bg: "url('./assets/bg.jpg')",
+    },
   },
   plugins: [require("daisyui")],
 };
